f34a263c3dd5e3733cec1aaaa7089ed1e2ce9316
Mani Movassagh <mani.mgh@gmail.com>
2025-07-15 01:05:58 +0200

    enhance mycat_plus to support line numbering in output

--- mycat_plus.c
+++ mycat_plus.c
@@ -1,11 +1,12 @@
-// mycat_plus.c
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #define BUF_SIZE 1024
 
-void print_file(const char *filename) {
+void print_file(const char *filename, int line_numbers) {
     int fd = open(filename, O_RDONLY);
     if (fd < 0) {
         write(2, "Cannot open file\n", 17);
@@ -14,22 +15,45 @@ void print_file(const char *filename) {
 
     char buffer[BUF_SIZE];
     ssize_t bytesRead;
+    int line_number = 1;
+    int new_line = 1; // flag to track line start
 
     while ((bytesRead = read(fd, buffer, BUF_SIZE)) > 0) {
-        write(1, buffer, bytesRead);
+        for (ssize_t i = 0; i < bytesRead; i++) {
+            if (new_line && line_numbers) {
+                char line_buf[32];
+                int len = snprintf(line_buf, sizeof(line_buf), "%6d  ", line_number++);
+                write(1, line_buf, len);
+                new_line = 0;
+            }
+
+            write(1, &buffer[i], 1);
+
+            if (buffer[i] == '\n') {
+                new_line = 1;
+            }
+        }
     }
 
     close(fd);
 }
 
 int main(int argc, char *argv[]) {
+    int line_numbers = 0;
+    int start_index = 1;
+
     if (argc < 2) {
-        write(2, "Usage: ./mycat_plus <file1> [file2...]\n", 40);
+        write(2, "Usage: ./mycat_plus [-n] <file1> [file2...]\n", 45);
         return 1;
     }
 
-    for (int i = 1; i < argc; i++) {
-        print_file(argv[i]);
+    if (strcmp(argv[1], "-n") == 0) {
+        line_numbers = 1;
+        start_index = 2;
+    }
+
+    for (int i = start_index; i < argc; i++) {
+        print_file(argv[i], line_numbers);
     }
 
     return 0;
