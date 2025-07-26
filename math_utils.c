6b7d8b69be095d8d68a958b33f232f31bd7dd3f8
Mani Movassagh <mani.mgh@gmail.com>
2025-07-27 00:00:11 +0200

    feat: add initial math_utils module with header and source files

---
+++ math_utils.c
@@ -0,0 +1,3 @@
+#include "math_utils.h"
+
+// Math utility function implementations will go here
---
+++ math_utils.h
@@ -0,0 +1,6 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+// Math utility function prototypes will go here
+
+#endif // MATH_UTILS_H
