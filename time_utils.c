283a442fc29a5dc74fee784df314f339c649d42e
Mani Movassagh <mani.mgh@gmail.com>
2025-07-27 21:01:23 +0200

    feat: add initial time_utils module with header and source files

---
+++ time_utils.c
@@ -0,0 +1,3 @@
+#include "time_utils.h"
+
+// Time utility function implementations will go here
---
+++ time_utils.h
@@ -0,0 +1,6 @@
+#ifndef TIME_UTILS_H
+#define TIME_UTILS_H
+
+// Time utility function prototypes will go here
+
+#endif // TIME_UTILS_H
