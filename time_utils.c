3662d2dbd87cbd1dac1c9cf1adf7c00b42ad79a5
Mani Movassagh <mani.mgh@gmail.com>
2025-07-27 21:01:42 +0200

    feat: add and implement get_current_time function in time_utils

--- time_utils.c
+++ time_utils.c
@@ -1,3 +1,8 @@
 #include "time_utils.h"
+#include <time.h>
 
 // Time utility function implementations will go here
+
+time_t get_current_time() {
+    return time(NULL);
+}
--- time_utils.h
+++ time_utils.h
@@ -1,6 +1,10 @@
 #ifndef TIME_UTILS_H
 #define TIME_UTILS_H
 
+#include <ctime> // Include ctime for time_t definition
+
 // Time utility function prototypes will go here
+
+time_t get_current_time();
 
 #endif // TIME_UTILS_H
