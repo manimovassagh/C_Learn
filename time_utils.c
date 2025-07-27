b8bf0d36ec2b0ad834d411c7baa74bd064e06498
Mani Movassagh <mani.mgh@gmail.com>
2025-07-27 21:01:54 +0200

    feat: add and implement format_time function in time_utils

--- time_utils.c
+++ time_utils.c
@@ -1,8 +1,15 @@
 #include "time_utils.h"
 #include <time.h>
+#include <stdio.h>
 
 // Time utility function implementations will go here
 
 time_t get_current_time() {
     return time(NULL);
+}
+
+char *format_time(time_t t, char *buf, size_t buflen) {
+    struct tm *tm_info = localtime(&t);
+    strftime(buf, buflen, "%Y-%m-%d %H:%M:%S", tm_info);
+    return buf;
 }
--- time_utils.h
+++ time_utils.h
@@ -6,5 +6,6 @@
 // Time utility function prototypes will go here
 
 time_t get_current_time();
+char *format_time(time_t t, char *buf, size_t buflen);
 
 #endif // TIME_UTILS_H
