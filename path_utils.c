a83cc22ea7c700c65074e4e11a07e15f4d2b3ca7
Mani Movassagh <mani.mgh@gmail.com>
2025-07-27 21:04:48 +0200

    feat: add and implement is_absolute_path function in path_utils

--- path_utils.c
+++ path_utils.c
@@ -1,3 +1,8 @@
 #include "path_utils.h"
+#include <string.h>
 
 // Path utility function implementations will go here
+
+int is_absolute_path(const char *path) {
+    return path && path[0] == '/';
+}
--- path_utils.h
+++ path_utils.h
@@ -3,4 +3,6 @@
 
 // Path utility function prototypes will go here
 
+int is_absolute_path(const char *path);
+
 #endif // PATH_UTILS_H
