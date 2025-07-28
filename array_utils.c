9fb73d82dbd19df4dea8e04f9f4d9675ee310696
Mani Movassagh <mani.mgh@gmail.com>
2025-07-29 00:01:56 +0200

    feat: add and implement array_sum function in array_utils

--- array_utils.c
+++ array_utils.c
@@ -1,3 +1,11 @@
 #include "array_utils.h"
 
 // Array utility function implementations will go here
+
+int array_sum(const int *arr, int len) {
+    int sum = 0;
+    for (int i = 0; i < len; ++i) {
+        sum += arr[i];
+    }
+    return sum;
+}
--- array_utils.h
+++ array_utils.h
@@ -3,4 +3,6 @@
 
 // Array utility function prototypes will go here
 
+int array_sum(const int *arr, int len);
+
 #endif // ARRAY_UTILS_H
