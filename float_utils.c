d8562756acb7a89db49d330c8a729d2b133f377c
Mani Movassagh <mani.mgh@gmail.com>
2025-07-30 00:01:56 +0200

    feat: add and implement float_min function in float_utils

--- float_utils.c
+++ float_utils.c
@@ -5,3 +5,7 @@
 double float_abs(double x) {
     return x < 0 ? -x : x;
 }
+
+double float_min(double a, double b) {
+    return a < b ? a : b;
+}
--- float_utils.h
+++ float_utils.h
@@ -2,6 +2,8 @@
 #define FLOAT_UTILS_H
 
 // Float utility function prototypes will go here
+
 double float_abs(double x);
+double float_min(double a, double b);
 
 #endif // FLOAT_UTILS_H
