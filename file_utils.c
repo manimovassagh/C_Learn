4d48efa4877bad6d389204b0f174c1e9d08d9b6b
Mani Movassagh <mani.mgh@gmail.com>
2025-08-01 00:36:35 +0200

    feat: add get_preferred_cache_block_size function for cache block size

--- file_utils.c
+++ file_utils.c
@@ -493,3 +493,10 @@ long get_preferred_readahead_block_size(const char *filename) {
     if (stat(filename, &st) < 0) return -1;
     return (long)st.st_blksize;
 }
+
+// Returns the file's preferred block size for cache, or -1 on error
+long get_preferred_cache_block_size(const char *filename) {
+    struct stat st;
+    if (stat(filename, &st) < 0) return -1;
+    return (long)st.st_blksize;
+}
