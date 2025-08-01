d54a4b9ed391f91775e91e74de13d4066ed524f2
Mani Movassagh <mani.mgh@gmail.com>
2025-08-01 14:19:31 +0200

    feat: add get_preferred_mirror_block_size function for mirror block size

--- file_utils.c
+++ file_utils.c
@@ -535,3 +535,10 @@ long get_preferred_backup_block_size(const char *filename) {
     if (stat(filename, &st) < 0) return -1;
     return (long)st.st_blksize;
 }
+
+// Returns the file's preferred block size for mirror, or -1 on error
+long get_preferred_mirror_block_size(const char *filename) {
+    struct stat st;
+    if (stat(filename, &st) < 0) return -1;
+    return (long)st.st_blksize;
+}
