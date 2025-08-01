2a40cd2b5bb1442e1045531fc3a9e9a212b6dafe
Mani Movassagh <mani.mgh@gmail.com>
2025-08-01 14:19:53 +0200

    feat: add get_preferred_compression_block_size function for compression block size

--- file_utils.c
+++ file_utils.c
@@ -542,3 +542,10 @@ long get_preferred_mirror_block_size(const char *filename) {
     if (stat(filename, &st) < 0) return -1;
     return (long)st.st_blksize;
 }
+
+// Returns the file's preferred block size for compression, or -1 on error
+long get_preferred_compression_block_size(const char *filename) {
+    struct stat st;
+    if (stat(filename, &st) < 0) return -1;
+    return (long)st.st_blksize;
+}
