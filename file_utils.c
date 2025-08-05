a13e6aef848600f486ae51c464a5aecc42d1ab8c
Mani Movassagh <mani.mgh@gmail.com>
2025-08-06 00:17:20 +0200

    feat: add get_preferred_image_block_size function for image files

--- file_utils.c
+++ file_utils.c
@@ -654,3 +654,10 @@ long get_preferred_font_block_size(const char *filename) {
     if (stat(filename, &st) < 0) return -1;
     return (long)st.st_blksize;
 }
+
+// Returns the file's preferred block size for image files, or -1 on error
+long get_preferred_image_block_size(const char *filename) {
+    struct stat st;
+    if (stat(filename, &st) < 0) return -1;
+    return (long)st.st_blksize;
+}
