57d947290ab47328e3099b854923482d2a3ceb9b
Mani Movassagh <mani.mgh@gmail.com>
2025-08-06 00:16:37 +0200

    feat: add get_preferred_font_block_size function for font files

--- file_utils.c
+++ file_utils.c
@@ -647,3 +647,10 @@ long get_preferred_resource_block_size(const char *filename) {
     if (stat(filename, &st) < 0) return -1;
     return (long)st.st_blksize;
 }
+
+// Returns the file's preferred block size for font files, or -1 on error
+long get_preferred_font_block_size(const char *filename) {
+    struct stat st;
+    if (stat(filename, &st) < 0) return -1;
+    return (long)st.st_blksize;
+}
