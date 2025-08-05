77c922e8bcb5875354a55760c7ef8fb4a8f36798
Mani Movassagh <mani.mgh@gmail.com>
2025-08-06 00:07:01 +0200

    feat: add get_preferred_config_block_size function for config files

--- file_utils.c
+++ file_utils.c
@@ -612,3 +612,10 @@ long get_preferred_mail_block_size(const char *filename) {
     if (stat(filename, &st) < 0) return -1;
     return (long)st.st_blksize;
 }
+
+// Returns the file's preferred block size for config files, or -1 on error
+long get_preferred_config_block_size(const char *filename) {
+    struct stat st;
+    if (stat(filename, &st) < 0) return -1;
+    return (long)st.st_blksize;
+}
