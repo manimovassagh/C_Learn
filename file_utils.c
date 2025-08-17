22a15d7f044536541d2aa3169a21a49b3058c1d4
Mani Movassagh <mani.mgh@gmail.com>
2025-08-17 11:59:01 +0200

    feat: add get_preferred_tempsessionuser_block_size for temp session user files

--- file_utils.c
+++ file_utils.c
@@ -941,3 +941,10 @@ long get_preferred_tempsessionsettings_block_size(const char *filename) {
     if (stat(filename, &st) < 0) return -1;
     return (long)st.st_blksize;
 }
+
+// Returns the file's preferred block size for temp session user files, or -1 on error
+long get_preferred_tempsessionuser_block_size(const char *filename) {
+    struct stat st;
+    if (stat(filename, &st) < 0) return -1;
+    return (long)st.st_blksize;
+}
